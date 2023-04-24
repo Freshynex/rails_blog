1ea96bb9ff10bfea78dcfaffb136b0998a136f9a
Freshynex <bence.recsnik@gmail.com>
2023-04-24 10:38:32 +0200

    Completed up to 9.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  resources :articles do
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
